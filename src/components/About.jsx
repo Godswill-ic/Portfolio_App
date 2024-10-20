061cae8def05d4e8ceabc5488f34159fd7eaa61f
Godswill-ic <gchukwueke01@gmail.com>
2024-10-20 13:37:37 +0100

    Updated coomit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <section id='about'>
         <h2 className='my-10 text-center text-4xl lg:text-7xl'>About Me</h2>
-        <div className='flex items-center justify-center'>
+        <div className='flex items-center justify-center text-center'>
             <p className='mx-8 lg:my-4 max-w-6xl text-[20px] text-gray-400 lg:text-3xl'>As a dedicated Full Stack Developer, I specialize in creating dynamic and responsive web applications that provide seamless user experiences. With a strong foundation in both front-end and back-end technologies, I excel in building robust and scalable solutions. My expertise includes working with JavaScript frameworks such as React and Node.js, as well as proficiency in databases like MongoDB and SQL. I am passionate about continuous learning and keeping up-to-date with the latest industry trends, which allows me to implement modern practices and tools in my projects.</p>
         </div>
     </section>
